f14c6e28e29e0d38cec7994535fa158042d56dde
ras5014 <ramanandasamantaray@gmail.com>
2024-01-03 20:47:43 +0530

    Initial commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <section className='w-full md:max--w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 
+      h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700'>
+        <h1>Weather Forecast</h1>
+
+        <p>Enter below a place you want to know the weather of and select an option from dropdown</p>
+      </section>
+   </main> 
+  );  
 }
 
 export default App;
